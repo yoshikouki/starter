ef4e99913ea47607803886fbdcad71d9df9643ce
yoshikouki <yoshikouki@pepabo.com>
2021-03-18 22:25:44 +0900

    Dockerに対応させてみる

--- script.ts
+++ script.ts
@@ -4,11 +4,12 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  const post = await prisma.post.update({
-    where: { id: 2 },
-    data: { published: true },
-  })
-  console.log(post)
+  // const post = await prisma.post.update({
+  //   where: { id: 2 },
+  //   data: { published: true },
+  // })
+  // console.log(post)
+
   // const post = await prisma.post.create({
   //   data: {
   //     title: 'Prisma make databases easy',
@@ -18,11 +19,9 @@ async function main() {
   //   },
   // })
   // console.log(post)
-  //
-  // const allUsers = await prisma.user.findMany({
-  //   include: { posts: true }
-  // })
-  // console.dir(allUsers, {depth: null})
+
+  const allUsers = await prisma.user.findMany()
+  console.dir(allUsers, {depth: null})
 }
 
 main()
